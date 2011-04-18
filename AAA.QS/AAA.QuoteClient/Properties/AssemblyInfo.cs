9b3354e9be7a0a46c18bd5803f98502d68998509
leokao.tw <leokao.tw@gmail.com>
2011-04-18 08:12:15 +0000

    Change AAA.QuoteClient Name

--- AAA.QS/AAA.QuoteClient/Properties/AssemblyInfo.cs
+++ AAA.QS/AAA.QuoteClient/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QuoteClient")]
+[assembly: AssemblyTitle("AAA.QuoteClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NCTU")]
-[assembly: AssemblyProduct("QuoteClient")]
+[assembly: AssemblyProduct("AAA.QuoteClient")]
 [assembly: AssemblyCopyright("Copyright © NCTU 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
